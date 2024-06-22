bb3278db80ca84c09a194ae311b2ca6e3ec18ac3
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-06-22 14:15:55 -0700

    added some codes

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -3,7 +3,14 @@ from test_framework import generic_test
 
 def reverse(x: int) -> int:
     # TODO - you fill in here.
-    return 0
+    result = 0
+    copy = abs(x)
+    while copy:
+        temp = copy % 10
+        result = result * 10 + temp
+        copy = copy // 10
+
+    return result if x > 0 else -result 
 
 
 if __name__ == '__main__':
